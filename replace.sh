b3f34b6f83befb264a4c43903d2663119520b8a9
byounggeol.cho <byounggeol.cho@nhn.com>
2023-03-13 11:40:15 +0900

    9709 [CloudTrail-Doc] 링크때문에 공공, ngsc 용 api guide, console-guide 생성

---
+++ replace.sh
@@ -0,0 +1,8 @@
+sed -e "s/event-list/event-list-gov/g" ko/api-guide.md > ko/api-guide-gov.md
+sed -e "s/event-list/event-list-ngsc/g" ko/api-guide.md > ko/api-guide-ngsc.md
+
+sed -e "s/s3-api-guide/s3-api-guide-gov/g" ko/console-guide.md > ko/console-guide-gov.md
+sed -e "s/s3-api-guide/s3-api-guide-ngsc/g" ko/console-guide.md > ko/console-guide-ngsc.md
+
+
+
