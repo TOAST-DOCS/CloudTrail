70bb5ae62fba4076c0cfb62c8dc4ab0faaaf3319
황병하/프레임워크개발팀/CL <byungha.hwang@nhn.com>
2024-11-29 10:14:55 +0900

    24/11/29 - ppp 추가

--- replace.sh
+++ replace.sh
@@ -1,12 +1,14 @@
 sed -e "s/event-list/event-list-gov/g" ko/api-guide.md > ko/api-guide-gov.md
+sed -e "s/\/Governance%20&%20Audit//g" ko/api-guide.md > ko/api-guide-ppp.md
+sed -e "s/event-list/event-list-ppp/g" ko/api-guide.md > ko/api-guide-ppp.md
 sed -e "s/\/Governance%20&%20Audit//g" ko/api-guide.md > ko/api-guide-ngsc.md
 sed -e "s/event-list/event-list-ngsc/g" ko/api-guide.md > ko/api-guide-ngsc.md
 
-
+git add ko/api-guide-gov.md ko/api-guide-ppp.md
 git add ko/api-guide-gov.md ko/api-guide-ngsc.md
 
 sed -e "s/s3-api-guide/s3-api-guide-gov/g" ko/console-guide.md > ko/console-guide-gov.md
 
-git add ko/console-guide-gov.md ko/console-guide-ngsc.md
+git add ko/console-guide-gov.md ko/console-guide-ppp.md ko/console-guide-ngsc.md
 
 
